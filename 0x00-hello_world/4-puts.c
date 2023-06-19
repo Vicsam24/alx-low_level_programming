1e678a23fd8581b0ac6f87247ce9f242349bd958
VicTM <vohanga24@gmail.com>
2023-06-19 23:58:16 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /*
 *main -main block
@@ -6,11 +5,6 @@
 */
 int main(void)
 {
-
-
-
-
 puts("\"Programming is like building a multilingual puzzle") 
-
-return (0);
+:return (0);
 }
